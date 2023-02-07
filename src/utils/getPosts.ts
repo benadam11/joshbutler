d6f65aad2bc0520babb3d82bb294e021efd80d14
Benjamin Adam <benjamin.mark.adam@gmail.com>
2023-02-07 10:44:42 -0700

    Use https

--- src/utils/getPosts.ts
+++ src/utils/getPosts.ts
@@ -3,7 +3,7 @@ import sanitize from "./sanitize";
 
 export async function getPosts() {
   const posts: any[] = await fetch(
-    "http://api.joshuaryanbutler.com/wp-json/wp/v2/posts?per_page=100"
+    "https://api.joshuaryanbutler.com/wp-json/wp/v2/posts?per_page=100"
   ).then((res) => res?.json());
   return posts.map((post: any) => ({
     ...post,
