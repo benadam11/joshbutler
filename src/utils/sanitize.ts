ccccee0627f23ed21975ed522eba79f465ebcdce
Benjamin Adam <benjamin.mark.adam@gmail.com>
2023-05-04 11:29:09 -0700

    Fix sanitize and blockquotes

--- src/utils/sanitize.ts
+++ src/utils/sanitize.ts
@@ -3,10 +3,11 @@ import sanitizeHtml from "sanitize-html";
 export default function sanitize(text: string) {
   return sanitizeHtml(text, {
     textFilter: (text: string) => text.replace(/&amp;/g, "&"),
-    allowedTags: sanitizeHtml.defaults.allowedTags.concat([ 'img', 'iframe' ]),
+    allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img", "iframe"]),
     // allowedIframeHostnames: ['www.youtube.com', 'player.vimeo.com'],
     allowedAttributes: {
-      iframe: ['src']
-  }
+      iframe: ["src"],
+      a: ["href", "name", "target"],
+    },
   });
 }
